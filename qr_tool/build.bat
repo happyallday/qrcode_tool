df3b8564b7156a2c1025a7631cf9a1ae42d65784
happyallday <happyallday121@163.com>
2026-03-26 14:04:17 +0800

    修复pyinstaller参数

--- qr_tool/build.bat
+++ qr_tool/build.bat
@@ -3,7 +3,7 @@ echo Installing dependencies...
 pip install PyInstaller==6.3.0
 
 echo Building exe...
-pyinstaller --onefile --windowed --name QRCodeTool --hidden-import=PyQt5 --hidden-import=qrcode --hidden-import=PIL --hidden-import=pyzbar.pyzbar --collect-all pyzbar main.py
+pyinstaller --onefile --windowed --name QRCodeTool --hidden-import=PyQt5 --hidden-import=qrcode --hidden-import=PIL --hidden-import=pyzbar.pyzbar --hidden-import=pyzbar --recursive-copy-recursive pyzbar main.py
 
 echo Done!
 echo Exe file: dist\QRCodeTool.exe
