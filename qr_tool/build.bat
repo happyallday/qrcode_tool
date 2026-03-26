fefa7279269d20ccccb15904c2be82476d96470a
happyallday <happyallday121@163.com>
2026-03-26 14:32:20 +0800

    修复删除spec文件问题

--- qr_tool/build.bat
+++ qr_tool/build.bat
@@ -5,7 +5,6 @@ pip install pyinstaller
 echo Cleaning previous build...
 if exist dist rmdir /s /q dist
 if exist build rmdir /s /q build
-if exist *.spec del /q *.spec
 
 echo Building exe...
 pyinstaller QRCodeTool.spec
