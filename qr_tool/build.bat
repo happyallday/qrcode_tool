ac85efe465461010651125f8f7ac5882f2a2ff65
happyallday <happyallday121@163.com>
2026-03-26 14:16:23 +0800

    添加PyQt5隐藏导入

--- qr_tool/build.bat
+++ qr_tool/build.bat
@@ -9,7 +9,7 @@ if exist build rmdir /s /q build
 if exist *.spec del /q *.spec
 
 echo Building exe...
-pyinstaller --onefile --console --name QRCodeTool --exclude-module pkg_resources main.py
+pyinstaller --onefile --console --name QRCodeTool --hidden-import=PyQt5 --hidden-import=PyQt5.QtCore --hidden-import=PyQt5.QtGui --hidden-import=PyQt5.QtWidgets --hidden-import=qrcode --hidden-import=PIL --hidden-import=pyzbar --hidden-import=pyzbar.pyzbar main.py
 
 echo Done!
 pause
