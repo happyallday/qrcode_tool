327e12bb98ae2f23289fa615247da22a1d636e4c
happyallday <happyallday121@163.com>
2026-03-26 14:04:29 +0800

    使用PyInstaller 5.x版本

--- qr_tool/build.bat
+++ qr_tool/build.bat
@@ -1,9 +1,9 @@
 @echo off
 echo Installing dependencies...
-pip install PyInstaller==6.3.0
+pip install PyInstaller==5.13.2
 
 echo Building exe...
-pyinstaller --onefile --windowed --name QRCodeTool --hidden-import=PyQt5 --hidden-import=qrcode --hidden-import=PIL --hidden-import=pyzbar.pyzbar --hidden-import=pyzbar --recursive-copy-recursive pyzbar main.py
+pyinstaller --onefile --windowed --name QRCodeTool main.py
 
 echo Done!
 echo Exe file: dist\QRCodeTool.exe
